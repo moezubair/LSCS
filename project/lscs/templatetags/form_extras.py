9a5ace7f1961d10e40873ef8f47dd033e8e1d53f
Greg Richardson <gregnr@uvic.ca>
2015-07-15 12:06:49 -0700

    Made username field autofocus and other minor changes

--- project/lscs/templatetags/form_extras.py
+++ project/lscs/templatetags/form_extras.py
@@ -3,8 +3,13 @@ from django import template
 register = template.Library()
 
 @register.filter
-def html_placeholder(field, args=None):
+def input_placeholder(field, args=None):
     if args is None:
         return field
     field.field.widget.attrs.update({ "placeholder": args })
+    return field
+
+@register.filter
+def input_autofocus(field):
+    field.field.widget.attrs.update({"autofocus":""})
     return field
